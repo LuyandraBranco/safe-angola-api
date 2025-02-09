9e5739b7e8816d6fe72af58ff7aa816b0386fcb0
LuyandraBranco <luyandrabranco@gmail.com>
2025-02-09 04:33:51 +0100

    feat: Controller user

--- src/interface/auth/auth.controller.ts
+++ src/interface/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Request,
+  UseGuards,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { AuthService } from '../../application/auth/auth.service';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
@@ -6,16 +16,43 @@ import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('create')
+  async createUser(@Body() body) {
+    return this.authService.createUser(body.username, body.password, body.role);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async getUser(@Param('id') id: string) {
+    return this.authService.getUserById(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  async updateUser(@Param('id') id: string, @Body() body) {
+    return this.authService.updateUser(id, body);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    return this.authService.deleteUser(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id/deactivate')
+  async deactivateUser(@Param('id') id: string) {
+    return this.authService.deactivateUser(id);
+  }
+
   @Post('login')
   async login(@Body() body: { username: string; password: string }) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return this.authService.validateUser(body.username, body.password);
   }
 
   @Post('profile')
   @UseGuards(JwtAuthGuard)
   getProfile(@Request() req) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return req.user;
   }
 }
