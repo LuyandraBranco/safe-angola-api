c74f5e68d2d55d4d0e8cb0e78d62036c7859bd96
luyandra.branco <luyandrabranco@gmail.com>
2025-03-31 15:14:28 +0100

    remove: AccidentModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth.module';
 import { UsersModule } from './modules/users.module';
-import { AccidentReportModule } from './modules/accident.report.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule, AccidentReportModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
   controllers: [],
   providers: [AuthModule, UsersModule],
 })
